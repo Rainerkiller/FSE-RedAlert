a9e79b462e80ffbebe1b09359941acf7cbc21767
Jacob <1524647495@qq.com>
2019-05-02 09:51:36 -0400

    Version 0.1.1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,22 +10,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Units BadTest = new Units();
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		System.out.println("1");
-
-
-		Units BadTest = new Units(50,"LIGHT",img,Sprite(null),"NOWEAPON");
+		Sprite Test = new Sprite(img);
+		BadTest.LoadUnits(50,"LIGHT",Test,"NOWEAPON");
 	}
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		BadTest.SetPostition(20, 20);
+		BadTest.ShowUnit(batch);
+		BadTest.MoveTo(50,50,batch);
 		batch.end();
 	}
 	
--- core/src/com/mygdx/game/Units.java
+++ core/src/com/mygdx/game/Units.java
@@ -2,20 +2,34 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import sun.swing.plaf.synth.DefaultSynthStyle;
+
+import java.util.Set;
 
 class Units {
     private int health;
     private String Armor;
-    private Texture StaticPicture;
+    private Sprite StaticPicture;
     private Sprite CurrentAction;
     private Sprite [] MovingActions;
     private String Weapon;
-    public void LoadUnit(int hp,String Arm,Texture StaticMotion ,Sprite[] AllMotion,String weapon){
+    public Units(){
+        health = 0;
+        Armor =null;
+        StaticPicture = null;
+        Weapon = null;
+    }
+    public Units(int hp,String Arm,Sprite StaticMotion ,String weapon){
         health = hp;
         Armor =Arm;
         StaticPicture = StaticMotion;
-        MovingActions = AllMotion;
+        Weapon = weapon;
+    }
+    public void LoadUnits(int hp,String Arm,Sprite StaticMotion ,String weapon){
+        health = hp;
+        Armor =Arm;
+        StaticPicture = StaticMotion;
         Weapon = weapon;
     }
     public int GetHealth(){
@@ -23,5 +37,25 @@ class Units {
     }
     public Sprite ShowMotion(){
         return CurrentAction;
+    }
+    public void ShowUnit(SpriteBatch batch){
+        StaticPicture.draw(batch);
+    }
+    public void SetPostition(float x,float y){
+        StaticPicture.setPosition(x,y);
+    }
+    public void MoveTo(float x,float y,SpriteBatch batch){
+        float CurrentX = StaticPicture.getX();
+        float CurrentY = StaticPicture.getY();
+        while(CurrentX<x&&CurrentY<y){
+            if(CurrentX<x){
+                CurrentX+=0.5;
+            }
+            if(CurrentY<y){
+                CurrentY+=0.5;
+            }
+            SetPostition(CurrentX,CurrentY);
+            ShowUnit(batch);
+        }
     }
 }
