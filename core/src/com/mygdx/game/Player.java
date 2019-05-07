8914a497418a7e17282afa0d0b6bf6bf65c91113
Handong Xue <xhd131142117@outlook.com>
2019-05-07 10:31:01 -0400

    sadada

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -3,13 +3,13 @@ package com.mygdx.game;
 import java.util.ArrayList;
 
 class Player {
-    private String Name;
+    private String name;
     private String Country;
-    private int Money;
-    private int MaxPower;
-    private int UsedPower;
-    private ArrayList<Buildings> Buildings;
-    private ArrayList<Units> Units;
+    private int money;
+    private int maxPower;
+    private int usedPower;
+    private ArrayList<Buildings> buildings;
+    private ArrayList<Units> units;
     private boolean LOSE = false;
     //-----------------Return Type-----------------------
 
