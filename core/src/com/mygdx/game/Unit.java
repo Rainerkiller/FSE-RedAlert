5845560a492ee9ab1faeb4f52d4f3e448de46a30
Handong Xue <xhd131142117@outlook.com>
2019-05-08 08:47:32 -0400

    Create Unit.java

---
+++ core/src/com/mygdx/game/Unit.java
@@ -0,0 +1,70 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+class Unit {
+    private int health;
+    private String name;
+    private String type;
+    private Sprite staticPicture;
+    private Sprite currentAction;
+    private Sprite [] movingActions;
+    private String weapon;
+
+    //--------------------Set up----------------------
+
+    public Unit(){
+        health = 0;
+        type = null;
+        staticPicture = null;
+        weapon = null;
+    }
+    public Unit(int hp,String Arm,Sprite StaticMotion ,String Weapon){
+        health = hp;
+        type = Arm;
+        staticPicture = StaticMotion;
+        weapon = Weapon;
+    }
+
+
+
+    //-----------------Return Type-----------------------
+
+    public int getHealth(){
+        return health;
+    }
+    public Sprite showMotion(){
+        return currentAction;
+    }
+
+    //-----------------Return Type-----------------------
+
+    public void loadUnits(int hp,String Arm,Sprite StaticMotion ,String Weapon){
+        health = hp;
+        type = Arm;
+        staticPicture = StaticMotion;
+        weapon = Weapon;
+    }
+    public void showUnit(SpriteBatch background){
+        staticPicture.draw(background);
+    }
+    public void setPosition(float x,float y){
+        staticPicture.setPosition(x,y);
+    }
+    public void MoveTo(float x,float y,SpriteBatch background){
+        float CurrentX = staticPicture.getX();
+        float CurrentY = staticPicture.getY();
+        while(CurrentX < x && CurrentY < y){
+            if(CurrentX < x){
+                CurrentX += 0.5;
+            }
+            if(CurrentY < y){
+                CurrentY += 0.5;
+            }
+            setPosition(CurrentX, CurrentY);
+            showUnit(background);
+        }
+    }
+
+}
