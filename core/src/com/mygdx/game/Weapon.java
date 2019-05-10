ddfe7b9ac7aed184151966d2c6a4242b46d44f52
Handong Xue <xhd131142117@outlook.com>
2019-05-10 10:36:59 -0400

    class toolbox

--- core/src/com/mygdx/game/Weapon.java
+++ core/src/com/mygdx/game/Weapon.java
@@ -5,35 +5,35 @@ import javafx.scene.effect.Light;
 import java.util.ArrayList;
 
 class Weapon {
-    private String Name;
-    private float[]TypeResistance = {1, 1, 1, 1};
-    private int Damage;
+    private String name;
+    private float[]typeResistance = {1, 1, 1, 1};
+    private int damage;
 
     //-----------------Set Up-----------------------
     public Weapon(){
-        Name = "NEWWEAPON";
-        Damage = 0;
+        name = "NEWWEAPON";
+        damage = 0;
     }
-    public Weapon(String name,float LightArmor,float MedArmor,float HeavyArmor,float Building,int Damage){
-        Name = name;
-        TypeResistance[0] = LightArmor;
-        TypeResistance[1] = MedArmor;
-        TypeResistance[2] = HeavyArmor;
-        TypeResistance[3] = Building;
-        this.Damage = Damage;
+    public Weapon(String Name,float lightArmor,float medArmor,float heavyArmor,float building,int Damage){
+        name = Name;
+        typeResistance[0] = lightArmor;
+        typeResistance[1] = medArmor;
+        typeResistance[2] = heavyArmor;
+        typeResistance[3] = building;
+        this.damage = Damage;
     }
 
     //-----------------Return Type-----------------------
 
-    public int GetDamage(){
-        return Damage;
+    public int getDamage(){
+        return damage;
     }
 
-    public String GetName(){
-        return Name;
+    public String getName(){
+        return name;
     }
 
-    public float[] GetTypeRsistance(){
-        return TypeResistance;
+    public float[] getTypeRsistance(){
+        return typeResistance;
     }
 }
