0414804b42648f40aaa1fa97018af75d0ca34923
Jacob <1524647495@qq.com>
2019-06-10 09:37:22 -0400

    AAAA

--- core/src/com/mygdx/game/loadingClass.java
+++ core/src/com/mygdx/game/loadingClass.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
 
 import java.util.ArrayList;
 
@@ -14,13 +15,16 @@ class loadingClass extends ApplicationAdapter {
     orl gold;
     orl dimand;
     public loadingClass(){
+
+    }
+    public loadingClass(String a){
         loadDirt();
         loadRole();
     }
     public void loadDirt(){
         ArrayList<Sprite> picture = new ArrayList<Sprite>();
-        picture.add(new Sprite(new Texture("pics/earth1.png")));
-        picture.add(new Sprite(new Texture("pics/earth2.png")));
+        picture.add(new Sprite(new Texture(Gdx.files.internal("pics/earth1.png"))));
+        picture.add(new Sprite(new Texture(Gdx.files.internal("pics/earth2.png"))));
         picture.add(new Sprite(new Texture("pics/earth3.png")));
         picture.add(new Sprite(new Texture("pics/earth4.png")));
         picture.add(new Sprite(new Texture("pics/earth5.png")));
